f28e4d72f1c7d3482c19f5dfb3ed24d063b7023e
Free Hong Kong <freehongkong.pp@gmail.com>
2020-09-20 12:16:01 +0800

    Added Xi Book template image

--- src/App.js
+++ src/App.js
@@ -1,57 +1,62 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect, useRef } from "react"
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
+  const [image, setImage] = useState(null)
+  const canvas = useRef(null)
+  const [topText, setTopText] = useState('')
+  const [bottomText, setBottomText] = useState('')
+
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+    const xiBookImageTemplate = new Image();
+    xiBookImageTemplate.src = "/images/XiBook_Template.jpg"
+    xiBookImageTemplate.onload = () => setImage(xiBookImageTemplate)
+  }, [])
+
+  useEffect(() => {
+    if (image && canvas) {
+      const ctx = canvas.current.getContext("2d")
+      ctx.fillStyle = "black"
+      ctx.fillRect(0, 0, 400, 256 + 80)
+      // ctx.drawImage(image, (400 - 256) / 2, 40)
+      ctx.drawImage(image,0,0)
+
+      ctx.font = "20px Comic Sans MS"
+      ctx.fillStyle = "white"
+      ctx.textAlign = "center"
+
+      ctx.fillText(topText, (400 / 2), 25)
+      ctx.fillText(bottomText, (400 / 2), 256 + 40 + 25)
+
     }
-    getDate();
-  }, []);
+  }, [image, canvas, topText, bottomText])
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+    <div>
+      <h1>Cat Meme!</h1>
+
+      <div>
+        <canvas
+          ref={canvas}
+          width={800}
+          height={256 + 80}
+        />
+      </div>
+
+      <div>
+        <input type="text"
+          value={topText}
+          onChange={e => setTopText(e.target.value)}
+        />
+        <br />
+        <input type="text"
+          value={bottomText}
+          onChange={e => setBottomText(e.target.value)}
+        />
+      </div>
+
+    </div>
+  )
 }
 
 export default App;
